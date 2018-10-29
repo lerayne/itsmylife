dad1b092ff1a7527516794380dd848ecb19f2fb3
Michael Yegorov <lerayne@gmail.com>
2018-10-29 20:43:02 +0200

    deletion of accidantally tracked config file; Login input form (raw draft)

--- src/shared/containers/LoginPage.jsx
+++ src/shared/containers/LoginPage.jsx
@@ -5,13 +5,30 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
+const errors = {
+    1: 'wrong login or password'
+}
+
 class LoginPage extends Component {
 
     static anonymousRequired = true
 
     render() {
         return <div>
-            Login Page
+
+            {this.props.location.query.error && <div>
+                Error: {errors[this.props.location.query.error] || this.props.location.query.error}
+            </div>}
+
+            <form method="POST" action="/login">
+                <div>
+                    <input type="text" name="email" placeholder="email" />
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="password" />
+                </div>
+                <input type="submit" value="Log in" />
+            </form>
 
             {this.props.children}
         </div>
